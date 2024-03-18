0a71c6b76903ad7b7025c8372d631b6658f3fad5
timfemey <isholaobafemi@gmail.com>
2024-03-18 13:04:13 +0100

    typescript and nodejs initialization

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import select, { Separator } from '@inquirer/select';
+
+const answer = await select({
+    message: 'Select a Rate Limiting Algorithm',
+    choices: [
+        {
+            name: 'token-bucket',
+            value: 'Token Bucket Algorithm',
+            description: 'A widely used rate limiting algorithm based on treating requests as tokens .',
+        },
+
+        new Separator(),
+        {
+            name: 'leak-bucket',
+            value: 'Leaking Bucket Algorithm',
+            description: 'Similar to Token bucket algorithm except that request are processed at a fixed rate',
+        },
+        {
+            name: 'fixed-window',
+            value: 'Fixed Window Counter Algorithm',
+            description: "The algorithm divides the timeline into fix-sized time windows and assign a counter for each window",
+            disabled: "(Not yet available)",
+        },
+        {
+            name: 'sliding-window',
+            value: 'Sliding Window Log Algorithm',
+            description: "The algorithm keeps track of request timestamps, on new request outdated timestamps are removed",
+            disabled: "(Not yet available)",
+        },
+    ],
+});
